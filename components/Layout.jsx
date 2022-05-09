7df87da66d4c6707fd2b293347e77a29ea645e8e
blockchaindev91 <hisamitsuteru@gmail.com>
2022-05-09 19:12:00 +0300

    Layout

---
+++ components/Layout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Kings Store Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
