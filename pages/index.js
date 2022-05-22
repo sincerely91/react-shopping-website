823d03a980f0ca247c26602ffda3e6c11d7a53e7
blockchaindev91 <hisamitsuteru@gmail.com>
2022-05-22 21:55:22 +0300

    index, Home

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
+import { client } from "../lib/client";
+
+const Home = ({ products, bannerData }) => (
+  <>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
+    <div className="products-heading">
+      <h2>Best Selling Products</h2>
+      <p>Eletronics of many variations</p>
+    </div>
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
